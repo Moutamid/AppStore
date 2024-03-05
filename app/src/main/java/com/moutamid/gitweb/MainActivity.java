55e2367dca793b84a0c18a2962cf8c1b23504531
Moutamid <lumianokia188@gmail.com>
2024-03-05 22:25:22 +0400

    added auto apk install dialog after downloading

--- app/src/main/java/com/moutamid/gitweb/MainActivity.java
+++ app/src/main/java/com/moutamid/gitweb/MainActivity.java
@@ -282,6 +282,7 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
                                 .execute(url);
                     }
                 })
+
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
